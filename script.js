afc8f9da30139677296b057454adb6ae4e46d47c
Oleksandr <oleksanddr@protonmail.com>
2021-03-20 17:35:36 +0200

    Розроблена лоігка гри

--- script.js
+++ script.js
@@ -1,25 +1,45 @@
-let heading = document.getElementById('colourValue');
-let buttons = document.getElementsByClassName('colourButton');
 
 
-const setButtonColour = (inputButton, red, blue, green) => {
-     return inputButton.setAttribute('style', 'background-color: rgb('+ red +', '+ green +', '+ blue +');')
+const setButtonColour = (inputButton, red, green, blue) => {
+     inputButton.setAttribute('style', 'background-color: rgb('+ red +', '+ green +', '+ blue +');')
 }
 
 const makeColourValue = () => {
      return Math.round(Math.random() * 255)
 }
 
-for (let i = 0; i < buttons.length; i++) {
+let heading = document.getElementById('colourValue');
+let buttons = document.getElementsByClassName('colourButton');
+let answerMessage = document.getElementById('answer');
+let reset = document.getElementById('resetButton');
 
-     let randomRed = makeColourValue();
-     let randomGreen = makeColourValue();
-     let randomBlue = makeColourValue();
-   
-     setButtonColour(buttons[i], randomRed, randomGreen, randomBlue);
-   
+
+const startGame = () => {
+     answerMessage.innerHTML = "";
+     
+     let answerButton = Math.round(Math.random() * (buttons.length - 1));
+     
+     for (let i = 0; i <= buttons.length; i++) {
+          
+          let randomRed = makeColourValue();
+          let randomGreen = makeColourValue();
+          let randomBlue = makeColourValue();
+          
+          setButtonColour(buttons[i], randomRed, randomGreen, randomBlue);
+          
+          if(i === answerButton){
+               heading.innerHTML = `(${randomRed}, ${randomGreen}, ${randomBlue})`;
+          }
+          
+          buttons[i].addEventListener('click', function(){
+               if(this === buttons[answerButton]){
+                    answerMessage.innerHTML = 'Правильно!'
+               } else {
+                    answerMessage.innerHTML = 'Колір вгадано не правильно'
+               }
+          });
+     }
 }
 
-
-setButtonColour(buttons[0], randomRed, randomGreen, randomBlue);
-console.log('ok');
+reset.addEventListener('click', startGame);
+startGame();
